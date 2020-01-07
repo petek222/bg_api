f0945c7f632c79dc3ec12fb2dab00a9ed0eab8de
Peter Koncelik <peter.e.koncelik@vanderbilt.edu>
2020-01-06 21:21:41 -0600

    setup

---
+++ src/server.js
@@ -0,0 +1,4 @@
+// Fill in with API processig information as dev continues. Basic JS currently housed in index.html file
+
+// Also note that the current server bootup requires navigation to the 'cors-anywhere' npm module, and then a basic node server.js of 
+// the module's own js file. This will hopefully be remedied soon. 
